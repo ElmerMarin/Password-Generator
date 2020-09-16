0a900f55bcabaf5b92005de289e3dc910f173880
Elmer Marin <elmermarindc@gmail.com>
2020-09-16 11:59:04 -0500

    Avances del Proyecto...90%

---
+++ WebAppPasswordG/Controllers/PasswordController.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppPasswordG.Controllers
+{
+    public class PasswordController : Controller
+    {
+        // GET: Password
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(bool? numeros, bool? minusculas, bool? mayusculas, bool? simbolos, string longitud)
+        {
+
+            Random rdn = new Random();
+            string cadena = "";
+            string Minusculas = "";
+            string Mayusculas = "";
+            string Numeros = "1234567890";
+            string Simbolos = "%$#@<>?¡¿()+-*/:.;,{}|¬&#!_-";
+
+            for (int i = 97; i < 123; i++)
+            {
+                Minusculas = Minusculas + char.ConvertFromUtf32(i);
+
+
+            }
+
+            for (int i = 65; i < 91; i++)
+            {
+                Mayusculas = Mayusculas + char.ConvertFromUtf32(i);
+
+
+            }
+
+
+            if (numeros == true)
+            {
+                cadena = cadena + Numeros;
+
+            }
+
+            if (minusculas == true)
+                cadena = cadena + Minusculas;
+
+            {
+
+            }
+            if (mayusculas == true)
+                cadena = cadena + Mayusculas;
+            {
+
+
+            }
+            if (simbolos == true)
+            {
+                cadena = cadena + Simbolos;
+
+
+            }
+
+
+
+            int longitudcadena = cadena.Length;
+            char letra;
+            int longitudContrasenia = Convert.ToInt32(longitud);
+            string contraseniaAleatoria = "";
+            for (int i = 0; i < longitudContrasenia && longitudcadena>0; i++)
+            {
+                letra = cadena[rdn.Next(longitudcadena)];
+                contraseniaAleatoria += letra.ToString();
+            }
+
+
+            ViewBag.Result = contraseniaAleatoria;
+            return View();
+        }
+    }
+}
---
+++ WebAppPasswordG/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("WebAppPasswordG")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebAppPasswordG")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si ComVisible se establece en false, los componentes COM no verán los 
+// tipos de este ensamblado. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el Id. typelib cuando este proyecto esté expuesto a COM
+[assembly: Guid("f975980a-e35a-4ec3-b7e9-02be6ef6a925")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
